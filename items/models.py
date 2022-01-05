54c4d1b3f0e8c1d3ad5e814c462b6718f48389f8
ferchoellargo <fernxn.10@gmail.com>
2022-01-05 12:27:55 -0600

    second-commit

--- items/models.py
+++ items/models.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 import hashlib
 
+
 class Item(models.Model):
 	uniqueValue = models.CharField(max_length=40)
 	name = models.CharField(max_length=40)
